874ced26143f25a88940e2e9e27344b503a9340c
cheema5 <ali.hassancheema@gmail.com>
2019-12-30 23:35:12 +0500

    prepopulate users and questions table using model factories and Database Seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        // definition of each method
+        // It means each time a user is created, something(function) would be done as well
+
+        // there is a difference between create and make method
+        // create will insert data into the database while 
+        // make will create new objects and store them in memory
+        
+        factory(App\User::class, 3)->create()->each(function ($user){
+            $user->questions()
+                ->saveMany(
+                    factory(App\Question::class, rand(1,5))->make()
+                );
+        });
     }
 }
